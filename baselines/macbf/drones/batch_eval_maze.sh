ab691ed04d883ee490b8eeae99e53af1b63f6ef2
anisamsrh <nuranisahidayatulmasruroh@gmail.com>
2026-02-25 03:17:03 +0000

    change batch script

--- baselines/macbf/drones/batch_eval_maze.sh
+++ baselines/macbf/drones/batch_eval_maze.sh
@@ -2,14 +2,14 @@ source .env
 
 curl -s -X POST https://api.telegram.org/bot$BOT_ID/sendMessage -d chat_id=$USER_ID -d text="Batch Jobs MACBF Start: Running All Evaluation for Maze Environment"
 
-python evaluate.py --num_agents 4 --model_path models/model_save --env Maze
-python evaluate.py --num_agents 8 --model_path models/model_save --env Maze
-python evaluate.py --num_agents 16 --model_path models/model_save --env Maze
-python evaluate.py --num_agents 32 --model_path models/model_save --env Maze
-python evaluate.py --num_agents 64 --model_path models/model_save --env Maze
-python evaluate.py --num_agents 128 --model_path models/model_save --env Maze
-python evaluate.py --num_agents 256 --model_path models/model_save --env Maze
-python evaluate.py --num_agents 512 --model_path models/model_save --env Maze
-python evaluate.py --num_agents 1024 --model_path models/model_save --env Maze
+python evaluate.py --num_agents 4 --model_path models/model_save --env Maze --vis 1
+python evaluate.py --num_agents 8 --model_path models/model_save --env Maze --vis 1
+python evaluate.py --num_agents 16 --model_path models/model_save --env Maze --vis 1
+python evaluate.py --num_agents 32 --model_path models/model_save --env Maze --vis 1
+python evaluate.py --num_agents 64 --model_path models/model_save --env Maze --vis 1
+python evaluate.py --num_agents 128 --model_path models/model_save --env Maze --vis 1
+python evaluate.py --num_agents 256 --model_path models/model_save --env Maze --vis 1
+python evaluate.py --num_agents 512 --model_path models/model_save --env Maze --vis 1
+python evaluate.py --num_agents 1024 --model_path models/model_save --env Maze --vis 1
 
 curl -s -X POST https://api.telegram.org/bot$BOT_ID/sendMessage -d chat_id=$USER_ID -d text="Batch Jobs MACBF End: All Evaluation Completed for Maze Environment"
